0b3f9d206a1aa599f15ab242106419ac8089d4df
Elise T. <125393390+setsunaxe7@users.noreply.github.com>
2025-12-29 15:14:59 +0800

    Create HelloJava.java

---
+++ Module 1 - Introduction to Java/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
+
+// println() prints message to console
+// Inside "" is the printed message
+// Semicolon ; ends the statement
