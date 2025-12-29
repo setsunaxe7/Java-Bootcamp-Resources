4ee228d6881e3a725ec9ce28706875cf5f45f922
Elise T. <125393390+setsunaxe7@users.noreply.github.com>
2025-12-29 15:36:58 +0800

    Workbook 1.1 Cleared

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -1,2 +1,13 @@
 // I'm a comment. I have no impact on your code.
 // Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+
+// print multiple lines in one println statement: use \n to indicate a new line within the "" after your first sentence and so on
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Python.");
+        System.out.println("I spilled java all over my paper.");
+        System.out.println("My dog ate my Java.");
+    }
+}
--- Module 1 - Introduction to Java/HelloJava.java
+++ Module 1 - Introduction to Java/HelloJava.java
@@ -4,6 +4,20 @@ public class HelloJava {
     }
 }
 
+// all code needs to exist in a java class
+// main() is the entry point of the program
 // println() prints message to console
+// every println() call outputs a new line
 // Inside "" is the printed message
 // Semicolon ; ends the statement
+// Camelcase is used for naming classes and methods (See HelloJava and main)
+// Curly braces {} define the start and end of classes and methods
+// Public means accessible from other classes
+// Static means it belongs to the class, not instances
+// Void means it does not return any value
+// System is a built-in class
+// out is a static member of System class
+// println is a method of the out object
+// args is an array of command-line arguments
+
+// x: is an inlay hint (very useful for functions)
